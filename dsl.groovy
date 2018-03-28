baa5b203a755b15c67dd395131bf39c29d4fb58d
Alexey <shumilovy@mail.ru>
2018-03-28 17:47:43 +0300

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,7 +1,7 @@
-def gitURL = "https://github.com/LehaNoisy/groovy_DSL.git"
 def git = "LehaNoisy/groovy_DSL"
 def repo = "ashumilov"
 
+def gitURL = "https://github.com/LehaNoisy/groovy_DSL.git"
 def command = "git ls-remote -h $gitURL"
 
 def proc = command.execute()
@@ -16,77 +16,63 @@ def branches = proc.in.text.readLines().collect {
     it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
 }
 
-job("MNTLAB-ashumilov-main-build-job"){
-    description ('Building necessary jobs')
-    
-parameters {
-     choiceParam('BRANCH_NAME', ['ashumilov', 'master'], 'Select the branch')
+job("MNTLAB-ashumilov-main-build-job") {
+    parameters {
+	choiceParam('BRANCH_NAME', ['ashumilov', 'master'], '')
         activeChoiceParam('BUILDS_TRIGGER') {
-            description('Available options')
+            filterable()
             choiceType('CHECKBOX')
             groovyScript {
                 script('["MNTLAB-ashumilov-child1-build-job", "MNTLAB-ashumilov-child2-build-job", "MNTLAB-ashumilov-child3-build-job", "MNTLAB-ashumilov-child4-build-job"]')
             }
         }
     }
-
-scm {
+    scm {
         github(git, '$BRANCH_NAME')
-}
-    
-triggers {
+    }
+    triggers {
         scm('H/5 * * * *')
+    }
+    steps {
+        downstreamParameterized {
+            trigger('$BUILDS_TRIGGER') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+               parameters {
+                    currentBuild()
+		}
+	    }
+	}	
+        shell('chmod +x do.sh && ./do.sh > output.log && cat output.log && tar -czf ${BRANCH_NAME}_dsl_do.tar.gz output.log')
+    }
+    publishers { 
+	archiveArtifacts('output.log')
+    }
+
+
+
 }
 
-steps {
-    downstreamParameterized {
-        trigger('$BUILDS_TRIGGER') {
-            block {
-                buildStepFailure('FAILURE')
-                failure('FAILURE')
-                unstable('UNSTABLE')
-            }    
-            parameters{
-                currentBuild()
-            }
-        }
-    }   
-    shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt')
-}
-publishers { 
-  archiveArtifacts('output.txt')
-}
-}
-
-1.upto(4){
+1.upto(4) {
 job("MNTLAB-ashumilov-child${it}-build-job") {
-    description "Creating children jobs"
     parameters {
-    choiceParam('BRANCH_NAME', branches)
+	choiceParam('BRANCH_NAME', branches, '')
     }
     scm {
         github(git, '$BRANCH_NAME')
     }
-steps {    
-    copyArtifacts('MNTLAB-ashumilov-main-build-job') {
-        includePatterns('script.sh')
-        targetDirectory('./')
-        flatten()
-        optional()
-        buildSelector {
-        latestSuccessful(true)
-        }
+    steps {
+        shell('chmod +x do.sh && ./do.sh > output.log && cat output.log && tar -czf  ${BRANCH_NAME}_dsl_do.tar.gz output.log jobs.groovy do.sh')
     }
-}
-steps {
-    shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf  ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy script.sh')
-}
-publishers {
+    publishers { 
         archiveArtifacts {
-            pattern('output.txt')
-            pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+            pattern('output.log')
+            pattern('${BRANCH_NAME}_dsl_do.tar.gz')
             onlyIfSuccessful()
-            }
-        }
-    }
+   }
+  }
+ }
 }
