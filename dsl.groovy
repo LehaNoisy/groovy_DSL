a45d15db2843351584bbc194de2e143b5a2b87bd
Alexey <shumilovy@mail.ru>
2018-03-28 17:04:25 +0300

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,18 +1,26 @@
-def command = "git ls-remote -h https://github.com/LehaNoisy/groovy_DSL.git" as java.lang.Object
+def gitURL = "https://github.com/LehaNoisy/groovy_DSL.git"
+def git = "LehaNoisy/groovy_DSL"
+def repo = "ashumilov"
+
+def command = "git ls-remote -h $gitURL"
+
 def proc = command.execute()
 proc.waitFor()
+
 if ( proc.exitValue() != 0 ) {
     println "Error, ${proc.err.text}"
     System.exit(-1)
 }
-idef branches = proc.in.text.readLines().collect {
+
+def branches = proc.in.text.readLines().collect {
     it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
 }
-job('EPBYMINW2470/MNTLAB-ashumilov-DSL-build-job') {
-    description 'Create child jobs.'
-    parameters {
-        //choiceParam(String parameterName, List<String> options, String description)
-        choiceParam('BRANCH_NAME', ['ashumilov', 'master'])
+
+job("MNTLAB-ashumilov-main-build-job"){
+    description ('Building necessary jobs')
+    
+parameters {
+     choiceParam('BRANCH_NAME', ['ashumilov', 'master'], 'Select the branch')
         activeChoiceParam('BUILDS_TRIGGER') {
             description('Available options')
             choiceType('CHECKBOX')
@@ -21,68 +29,64 @@ job('EPBYMINW2470/MNTLAB-ashumilov-DSL-build-job') {
             }
         }
     }
-        triggers {
-            scm('H/5 * * * *')
-        }
 
+scm {
+        github(git, '$BRANCH_NAME')
+}
+    
+triggers {
+        scm('H/5 * * * *')
+}
+
+steps {
+    downstreamParameterized {
+        trigger('$BUILDS_TRIGGER') {
+            block {
+                buildStepFailure('FAILURE')
+                failure('FAILURE')
+                unstable('UNSTABLE')
+            }    
+            parameters{
+                currentBuild()
+            }
+        }
+    }   
+    shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt')
+}
+publishers { 
+  archiveArtifacts('output.txt')
+}
+}
+
+1.upto(4){
+job("MNTLAB-ashumilov-child${it}-build-job") {
+    description "Creating children jobs"
+    parameters {
+    choiceParam('BRANCH_NAME', branches)
+    }
     scm {
-        github 'LehaNoisy/groovy_DSL','$BRANCH_NAME'
+        github(git, '$BRANCH_NAME')
     }
-    steps {
-        downstreamParameterized {
-            trigger('$BUILDS_TRIGGER') {
-                block {
-                    buildStepFailure('FAILURE')
-                    failure('FAILURE')
-                    unstable('UNSTABLE')
-                }
-                parameters {
-                    currentBuild()
-                }
-            }
+steps {    
+    copyArtifacts('MNTLAB-ashumilov-main-build-job') {
+        includePatterns('script.sh')
+        targetDirectory('./')
+        flatten()
+        optional()
+        buildSelector {
+        latestSuccessful(true)
         }
-        //shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt')
     }
-    publishers {
-        archiveArtifacts('output.txt')
-    }
-    }
-
-
-
-        //creating child jobs
-    1.upto(4) {
-        job("EPBYMINW2470/MNTLAB-ashumilov-child${it}-build-job") {
-            description 'Echo the shell.sh.'
-            parameters {
-                choiceParam('BRANCH_NAME', branches, 'Select git branch')
-                /*gitParam('BRANCH_NAME') {
-                    description('branch selection')
-                    type('BRANCH')
-                    //branch('~ /*')
-                    //defaultValue('/ashumilov')
-                    sortMode('ASCENDING')
-                }*/
-            }
-            scm {
-                git {
-                    remote {
-                        name('groovy_DSL')
-                        url('https://github.com/LehaNoisy/groovy_DSL.git')
-                    }
-                    branch('$BRANCH_NAME')
-                }
-                triggers {
-                    scm 'H/5 * * * *'
-                }
-            }
-            steps {
-                shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && ' +
-                        'tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy')
-            }
-            publishers {
-                archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz')
+}
+steps {
+    shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf  ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy script.sh')
+}
+publishers {
+        archiveArtifacts {
+            pattern('output.txt')
+            pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+            onlyIfSuccessful()
             }
         }
     }
-
+}
