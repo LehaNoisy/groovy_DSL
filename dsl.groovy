2ff3659f98dff5d574046c02033530b25744d2dc
Alexey <shumilovy@mail.ru>
2018-03-28 15:21:20 +0300

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,58 +1,80 @@
-job('EPBYMINW2470/MNTLAB-ashumilov-main-build-job'){
-  scm {
-        github ('LehaNoisy/groovy_DSL.git', '$BRANCH_NAME')
-     }      
-  parameters {
-        choiceParam('BRANCH_NAME', ['ashumilov', 'master'], 'select branch')
-	activeChoiceParam('BUILDS_TRIGGER') {
+tURL = "https://github.com/LehaNoisy/groovy_DSL.git"
+def git = "LehaNoisy/groovy_DSL"
+def repo = "master"
+
+job("main-build-job"){
+    description ('Building necessary jobs')
+    
+parameters {
+     choiceParam('BRANCH_NAME', ['ashumilov', 'master'], 'make your choice')
+        activeChoiceParam('BUILDS_TRIGGER'){ 
+        description('Allows user choose')
+            description('Available options')
             choiceType('CHECKBOX')
             groovyScript {
-                script('return ["MNTLAB-ashumilov-child1-build-job", "MNTLAB-ashumilov-child2-build-job", "MNTLAB-ashumilov-child3-build-job", "MNTLAB-ashumilov-child4-build-job"]')
+                script('["MNTLAB-ashumilov-child1-build-job", "MNTLAB-ashumilov-child2-build-job", "MNTLAB-ashumilov-child3-build-job", "MNTLAB-ashumilov-child4-build-job"]')
             }
         }
     }
-  steps {	
-	downstreamParameterized {   
-                trigger('$BUILDS_TRIGGER'){      
-                	block{
-                    	buildStepFailure('FAILURE')
-                    	failure('FAILURE')
-                    	unstable('UNSTABLE')
-                }
-        parameters {
-                    predefinedProp ('BRANCH_NAME', '$BRANCH_NAME')
-        }
-      }
+
+
+scm {
+        github 'LehaNoisy/groovy_DSL','$BRANCH_NAME'
     }
-  }
+  
+    steps {
+        downstreamParameterized {
+            trigger('$BUILDS_TRIGGER') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                parameters {
+                    currentBuild()
+                }
+            }
+        }
 }
 
-
-def gitURL = "https://github.com/LehaNoisy/groovy_DSL.git"
-def command = "git ls-remote -h -t $gitURL"
-def proc = command.execute()
-def branches = proc.in.text.readLines().collect
-        {
-            it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
-        }
-
-for (i = 1; i <5; i++) {
-  job ("EPBYMINW2470/MNTLAB-ashumilov-child${i}-build-job"){ 
-    parameters {
-        choiceParam('BRANCH_NAME', branches)
-    }		   
-    scm {
-         github ('LehaNoisy/groovy_DSL', '$BRANCH_NAME')
+   publishers {
+        archiveArtifacts('output.txt')
     }
-    steps {
-        shell('chmod +x ./script.sh; ./script.sh >> output.txt; tar -czvf "${BRANCH_NAME}_dsl_script.tar.gz" output.txt script.sh jobs.groovy')    	
- }
-    publishers {
-        archiveArtifacts {
-          pattern('output.txt')
-          pattern ('${BRANCH_NAME}_dsl_script.tar.gz')
-          onlyIfSuccessful()          
-       }
-     }
-   }
+}
+
+//creating child jobs
+    1.upto(4) {
+        job("EPBYMINW2470/MNTLAB-ashumilov-child${it}-build-job") {
+            description 'Echo the shell.sh.'
+            parameters {
+                choiceParam('BRANCH_NAME', branches, 'Select git branch')
+                /*gitParam('BRANCH_NAME') {
+                    description('branch selection')
+                    type('BRANCH')
+                    //branch('~ /*')
+                    //defaultValue('/ashumilov')
+                    sortMode('ASCENDING')
+                }*/
+            }
+            scm {
+                git {
+                    remote {
+                        name('mntlab-dsl')
+                        url('https://github.com/LehaNoisy/groovy_DSL.git')
+                    }
+                    branch('$BRANCH_NAME')
+                }
+                triggers {
+                    scm 'H/5 * * * *'
+                }
+            }
+            steps {
+                shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && ' +
+                        'tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy')
+            }
+            publishers {
+                archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz')
+            }
+        }
+    }
 }
