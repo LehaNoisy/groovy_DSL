62dec2e0881b637ef10695b9c365dbe06a09e2d9
Alexey <shumilovy@mail.ru>
2018-03-28 15:47:47 +0300

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,14 +1,30 @@
-tURL = "https://github.com/LehaNoisy/groovy_DSL.git"
+def gitURL = "https://github.com/LehaNoisy/groovy_DSL.git"
 def git = "LehaNoisy/groovy_DSL"
-def repo = "master"
+def repo = "ashumilov"
 
-job("main-build-job"){
+//def gitURL = "https://github.com/MNT-Lab/mntlab-dsl.git"
+//def git = "MNT-Lab/mntlab-dsl"
+//def repo = "pyurchuk"
+def command = "git ls-remote -h $gitURL"
+
+def proc = command.execute()
+proc.waitFor()
+
+if ( proc.exitValue() != 0 ) {
+    println "Error, ${proc.err.text}"
+    System.exit(-1)
+}
+
+def branches = proc.in.text.readLines().collect {
+    it.replaceAll(/[a-z0-9]*\trefs\/heads\//, '')
+}
+
+job("MNTLAB-ashumilov-main-build-job"){
     description ('Building necessary jobs')
     
 parameters {
-     choiceParam('BRANCH_NAME', ['ashumilov', 'master'], 'make your choice')
-        activeChoiceParam('BUILDS_TRIGGER'){ 
-        description('Allows user choose')
+     choiceParam('BRANCH_NAME', ['ashumilov', 'master'], 'Select the branch')
+        activeChoiceParam('BUILDS_TRIGGER') {
             description('Available options')
             choiceType('CHECKBOX')
             groovyScript {
@@ -17,56 +33,59 @@ parameters {
         }
     }
 
-
 scm {
-        github 'LehaNoisy/groovy_DSL','$BRANCH_NAME'
-    }
-  
-    steps {
-        downstreamParameterized {
-            trigger('$BUILDS_TRIGGER') {
-                block {
-                    buildStepFailure('FAILURE')
-                    failure('FAILURE')
-                    unstable('UNSTABLE')
-                }
-                parameters {
-                    currentBuild()
-                }
-            }
-        }
+        github(git, '$BRANCH_NAME')
 }
 
-   publishers {
-        archiveArtifacts('output.txt')
-    }
+steps {
+    downstreamParameterized {
+        trigger('$BUILDS_TRIGGER') {
+            block {
+                buildStepFailure('FAILURE')
+                failure('FAILURE')
+                unstable('UNSTABLE')
+            }    
+            parameters{
+                currentBuild()
+            }
+        }
+    }   
+    shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt')
+}
+publishers { 
+  archiveArtifacts('output.txt')
+}
 }
 
-//creating child jobs
-    1.upto(4) {
-        job("EPBYMINW2470/MNTLAB-ashumilov-child${it}-build-job") {
-            description 'Echo the shell.sh.'
-            parameters {
-                choiceParam('BRANCH_NAME', branches, 'Select git branch')
-            }
-            scm {
-                git {
-                    remote {
-                        name('LehaNoisy')
-                        url('https://github.com/LehaNoisy/groovy_DSL.git')
-                    }
-                    branch('$BRANCH_NAME')
-                }
-                triggers {
-                    scm 'H/5 * * * *'
-                }
-            }
-            steps {
-                shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && ' +
-                        'tar -czf ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy')
-            }
-            publishers {
-                archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz')
+1.upto(4){
+job("MNTLAB-ashumilov-child${it}-build-job") {
+    description "Creating children jobs"
+    parameters {
+    choiceParam('BRANCH_NAME', branches)
+    }
+    scm {
+        github(git, '$BRANCH_NAME')
+    }
+steps {    
+    copyArtifacts('MNTLAB-ashumilov-main-build-job') {
+        includePatterns('script.sh')
+        targetDirectory('./')
+        flatten()
+        optional()
+        buildSelector {
+        latestSuccessful(true)
+        }
+    }
+}
+steps {
+    shell('chmod +x script.sh && ./script.sh > output.txt && cat output.txt && tar -czf  ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy script.sh')
+}
+publishers {
+        archiveArtifacts {
+            pattern('output.txt')
+            pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+            onlyIfSuccessful()
             }
         }
     }
+}
